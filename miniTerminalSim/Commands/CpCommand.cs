59b85667d3c3d956dd98ea4bd370ea65300e1bf0
adrian.cholody <adriancholody@gmail.com>
2023-12-11 16:02:24 +0100

    Created cp command(not working for now)

---
+++ miniTerminalSim/Commands/CpCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniTerminalSim.Commands
+{
+    internal class CpCommand : CommandAbstract
+    {
+        public CpCommand(FileExplorer fileExplorer, string[] args) : base(fileExplorer, args) { }
+
+        public override string[] Execute()
+        {
+            if (args.Length < 2)
+            {
+                string text = "cp: missing file operand";
+                return new string[] { text };
+            }
+
+            return null;
+        }
+    }
+}
