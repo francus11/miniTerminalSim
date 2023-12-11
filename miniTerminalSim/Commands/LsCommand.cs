31aa7db82e7fbdcbe13ce97d5de2b1adc091a044
adrian.cholody <adriancholody@gmail.com>
2023-12-11 16:00:03 +0100

    added ls command (early stage)

---
+++ miniTerminalSim/Commands/LsCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniTerminalSim.Commands
+{
+    internal class LsCommand : CommandAbstract
+    {
+        public LsCommand(FileExplorer fileExplorer, string[] args) : base(fileExplorer, args) { }
+
+        public override string[] Execute()
+        {
+            if (args.Length == 0 || args == null)
+            {
+                string content = "";
+
+                foreach(var item in fileExplorer.CurrentScope.Components)
+                {
+                    content += $"{item.Name}  ";
+                }
+
+                return new string[] { content };
+            }
+
+            return null;
+        }
+    }
+}
