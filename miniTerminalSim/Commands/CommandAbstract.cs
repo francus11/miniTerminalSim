305e628f58282c1cb0d19ee9e9ba10abc60fd537
adrian.cholody <adriancholody@gmail.com>
2023-12-11 16:01:49 +0100

    Added method to handle finding directory easier

--- miniTerminalSim/Commands/CommandAbstract.cs
+++ miniTerminalSim/Commands/CommandAbstract.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using miniTerminalSim.FileSystem;
+using System.Xml.XPath;
 
 namespace miniTerminalSim.Commands
 {
@@ -18,5 +15,53 @@ namespace miniTerminalSim.Commands
         }
 
         public abstract string[] Execute();
+
+        protected Catalog SearchCatalogFromPath(string[] path)
+        {
+
+            //string[] path = pathLine.Split("/");
+            Catalog catalog;
+
+            if (path[0] == "")
+            {
+                catalog = fileExplorer.RootCatalog;
+                path = path[1..];
+            }
+            else
+            {
+                catalog = fileExplorer.CurrentScope;
+                return null;
+            }
+
+            try
+            {
+                foreach (string pathItem in path)
+                {
+                    if (pathItem == "..")
+                    {
+                        catalog = catalog.Parent;
+                    }
+                    else
+                    {
+                        catalog = (Catalog)catalog.Select(pathItem);
+                    }
+                }
+            }
+            catch (InvalidCastException)
+            {
+                throw;
+            }
+            catch(FileNotFoundException)
+            {
+                throw;
+            }
+
+            return catalog;
+        }
+
+        protected string[] PathLineToPath(string pathLine)
+        {
+            return pathLine.Split("/");
+        }
     }
 }
