638b00af016faaeb6d1fda220d97eae054f9e41b
adrian.cholody <adriancholody@gmail.com>
2023-12-12 13:07:18 +0100

    Implemented whole command

--- miniTerminalSim/Commands/CpCommand.cs
+++ miniTerminalSim/Commands/CpCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using miniTerminalSim.FileSystem;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,52 @@ namespace miniTerminalSim.Commands
             {
                 string text = "cp: missing file operand";
                 return new string[] { text };
+            }
+
+            Catalog catalog1 = null;
+            IFileSystemComponent copiedObject = null;
+
+
+            try
+            {
+                catalog1 = SearchCatalogFromPath(PathLineToPath(args[0])[..^1]);
+                copiedObject = (IFileSystemComponent)catalog1.Select(PathLineToPath(args[0])[^1]).Clone();
+            }
+            catch (InvalidCastException)
+            {
+                string text = "mv: can't find file or directory";
+                return new string[] { text };
+            }
+            catch (FileNotFoundException)
+            {
+                string text = "mv: can't find file or directory";
+                return new string[] { text };
+            }
+
+
+            Catalog catalog2 = null;
+
+            try
+            {
+                catalog2 = SearchCatalogFromPath(PathLineToPath(args[1])[..^1]);
+            }
+            catch (InvalidCastException)
+            {
+                string text = "mv: can't find file or directory";
+                return new string[] { text };
+            }
+            catch (FileNotFoundException)
+            {
+                string text = "mv: can't find file or directory";
+                return new string[] { text };
+            }
+
+            if (!catalog2.Add(copiedObject))
+            {
+                catalog2.Remove(copiedObject);
+                catalog2.Add(copiedObject);
+                copiedObject.Parent = catalog2;
+                copiedObject.Name = PathLineToPath(args[1])[-1];
             }
 
             return null;
