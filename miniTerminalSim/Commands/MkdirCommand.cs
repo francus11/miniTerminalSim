31a0a73133d0eb1beac5a9da9fb80344b41c7d21
adrian.cholody <adriancholody@gmail.com>
2023-12-12 13:03:19 +0100

    Added mkdir command

---
+++ miniTerminalSim/Commands/MkdirCommand.cs
@@ -0,0 +1,39 @@
+﻿using miniTerminalSim.FileSystem;
+
+namespace miniTerminalSim.Commands
+{
+    internal class MkdirCommand : CommandAbstract
+    {
+        public MkdirCommand(FileExplorer fileExplorer, string[] args) : base(fileExplorer, args) { }
+
+        public override string[] Execute()
+        {
+            if (args == null || args.Length == 0)
+            {
+                string text = "mkdir: missing operand";
+                return new string[] { text };
+            }
+            Catalog parentCatalog;
+            string[] path = PathLineToPath(args[0][..^1]);
+            if (path[..^1] == null || path[..^1].Length == 0)
+            {
+                parentCatalog = fileExplorer.CurrentScope;
+            }
+            else
+            {
+
+                parentCatalog = SearchCatalogFromPath(PathLineToPath(args[0])[..^1]);
+            }
+
+            Catalog newCatalog = new Catalog(PathLineToPath(args[0])[^1]);
+            newCatalog.Parent = parentCatalog;
+            if (!parentCatalog.Add(newCatalog))
+            {
+                string text = "mkdir: directory already exists";
+                return new string[] { text };
+            }
+
+            return null;
+        }
+    }
+}
